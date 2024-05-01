4c7a9385df3f60a0d5f545bf658dfa864569a4f9
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-05-01 11:01:12 -0400

    Initial commit

---
+++ com/example/RecBinarySearch.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-05-01
+ */
+
+// RecBinarySearch class
+public final class RecBinarySearch {
+
+    /** Private constructor to prevent instantiation. */
+    private RecBinarySearch() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
